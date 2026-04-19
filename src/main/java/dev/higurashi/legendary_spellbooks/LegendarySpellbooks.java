fc84b763d4e06702e3803c4aa583b5aab9252866
Higurashi34m <240164450+Higurashi34m@users.noreply.github.com>
2026-04-19 16:45:08 +0900

    Initial Commit

---
+++ src/main/java/dev/higurashi/legendary_spellbooks/LegendarySpellbooks.java
@@ -0,0 +1,20 @@
+package dev.higurashi.legendary_spellbooks;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(LegendarySpellbooks.MOD_ID)
+public class LegendarySpellbooks {
+    public static final String MOD_ID = "legendary_spellbooks";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public LegendarySpellbooks() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
