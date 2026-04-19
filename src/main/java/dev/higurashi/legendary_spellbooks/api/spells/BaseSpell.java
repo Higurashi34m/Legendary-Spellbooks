c59f767c383b135419f3f61f5d87dc3460e4ae85
Higurashi34m <240164450+Higurashi34m@users.noreply.github.com>
2026-04-19 18:07:37 +0900

    feat: Add BaseSpell class

---
+++ src/main/java/dev/higurashi/legendary_spellbooks/api/spells/BaseSpell.java
@@ -0,0 +1,50 @@
+package dev.higurashi.legendary_spellbooks.api.spells;
+
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import io.redspace.ironsspellbooks.api.spells.CastType;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.entity.LivingEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseSpell extends AbstractSpell {
+    private final ResourceLocation spellResource;
+    private final DefaultConfig spellConfig;
+    private final CastType castType;
+
+    protected SoundEvent castStartSound = null;
+    protected SoundEvent castFinishSound = null;
+
+    protected BaseSpell(ResourceLocation spellResource, DefaultConfig spellConfig, CastType castType) {
+        this.spellResource = spellResource;
+        this.spellConfig = spellConfig;
+        this.castType = castType;
+    }
+
+    protected BaseSpell(ResourceLocation spellResource, DefaultConfig spellConfig) {
+        this.spellResource = spellResource;
+        this.spellConfig = spellConfig;
+        this.castType = CastType.INSTANT;
+    }
+
+    @Override public ResourceLocation getSpellResource() { return spellResource; }
+    @Override public DefaultConfig getDefaultConfig() { return spellConfig; }
+
+    @Override public CastType getCastType() { return castType; }
+
+    @Override public Optional<SoundEvent> getCastStartSound() {
+        if (this.castStartSound == null) super.getCastStartSound();
+        return Optional.of(castStartSound);
+    }
+
+    @Override public Optional<SoundEvent> getCastFinishSound() {
+        if (this.castFinishSound == null) super.getCastFinishSound();
+        return Optional.of(castFinishSound);
+    }
+
+    @Override public abstract List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster);
+}
