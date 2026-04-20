f097990743d5936b2228c5e99ebabdf0dab52cf7
Higurashi34m <240164450+Higurashi34m@users.noreply.github.com>
2026-04-20 13:36:23 +0900

    refactor: Update onCast method to use consistent parameter naming for castSource

--- src/main/java/dev/higurashi/legendary_spellbooks/common/spells/fire/FlameEaterSpell.java
+++ src/main/java/dev/higurashi/legendary_spellbooks/common/spells/fire/FlameEaterSpell.java
@@ -70,12 +70,12 @@ public class FlameEaterSpell extends BaseSpell {
 
 
     @Override
-    public void onCast(Level level, int spellLevel, LivingEntity caster, CastSource castSource, MagicData magicData) {
+    public void onCast(Level level, int spellLevel, LivingEntity caster, CastSource source, MagicData magicData) {
         if (magicData.getAdditionalCastData() instanceof TargetEntityCastData targetEntityCastData) {
             PlayerRecasts recasts = magicData.getPlayerRecasts();
 
             if (!recasts.hasRecastForSpell(getSpellId())) {
-                recasts.addRecast(new RecastInstance(getSpellId(), spellLevel, getRecastCount(spellLevel, caster), 40, castSource, new MultiTargetEntityCastData(targetEntityCastData.getTarget((ServerLevel) level))), magicData);
+                recasts.addRecast(new RecastInstance(getSpellId(), spellLevel, getRecastCount(spellLevel, caster), 40, source, new MultiTargetEntityCastData(targetEntityCastData.getTarget((ServerLevel) level))), magicData);
             } else {
                 RecastInstance recast = recasts.getRecastInstance(this.getSpellId());
 
@@ -85,7 +85,7 @@ public class FlameEaterSpell extends BaseSpell {
             }
         }
 
-        super.onCast(level, spellLevel, caster, castSource, magicData);
+        super.onCast(level, spellLevel, caster, source, magicData);
     }
 
     @Override
