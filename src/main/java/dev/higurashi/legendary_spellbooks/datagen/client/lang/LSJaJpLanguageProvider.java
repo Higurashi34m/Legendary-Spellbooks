337506e732ef7b803dd65813bb1041b0945f91d8
Higurashi34m <240164450+Higurashi34m@users.noreply.github.com>
2026-04-20 09:05:46 +0900

    feat: Add Nimbus Array spell

--- src/main/java/dev/higurashi/legendary_spellbooks/datagen/client/lang/LSJaJpLanguageProvider.java
+++ src/main/java/dev/higurashi/legendary_spellbooks/datagen/client/lang/LSJaJpLanguageProvider.java
@@ -19,8 +19,12 @@ public class LSJaJpLanguageProvider extends BaseLanguageProvider {
         // Lightning
         addSpell(LSSpellRegistry.CLOUD_RAIL_SPELL, "クラウドレール", "自分の見ている方向に直線状に魔法の雲を召喚する。この雲は落下し、当たった位置から範囲ダメージを与える。");
         addSpell(LSSpellRegistry.CLOUD_RING_SPELL, "クラウドリング", "自分を中心に複数のリング状に魔法の雲を召喚する。この雲は落下し、当たった位置から範囲ダメージを与える。");
+        addSpell(LSSpellRegistry.NIMBUS_ARRAY_SPELL, "ニンバスアレー", "自分の見ている方向に直線状の雷を召喚する。この雷からは一定時間雷が落ち、当たった相手にダメージを与える。");
 
         // Death Attack
         addSpellDamageSource(LSSpellRegistry.CLOUD_RAIL_SPELL, "%1$sは %2$sの雲に巻き込まれて押しつぶされた");
+
+        // UI
+        addUi("nimbus_count", "雷雲の数: %d");
     }
 }
--- src/main/java/dev/higurashi/legendary_spellbooks/registries/LSSpellRegistry.java
+++ src/main/java/dev/higurashi/legendary_spellbooks/registries/LSSpellRegistry.java
@@ -3,6 +3,7 @@ package dev.higurashi.legendary_spellbooks.registries;
 import dev.higurashi.legendary_spellbooks.LegendarySpellbooks;
 import dev.higurashi.legendary_spellbooks.common.spells.lightning.CloudRailSpell;
 import dev.higurashi.legendary_spellbooks.common.spells.lightning.CloudRingSpell;
+import dev.higurashi.legendary_spellbooks.common.spells.lightning.NimbusArraySpell;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
 import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -17,4 +18,5 @@ public class LSSpellRegistry {
     // Lightning
     public static final RegistryObject<AbstractSpell> CLOUD_RAIL_SPELL = register(new CloudRailSpell());
     public static final RegistryObject<AbstractSpell> CLOUD_RING_SPELL = register(new CloudRingSpell());
+    public static final RegistryObject<AbstractSpell> NIMBUS_ARRAY_SPELL = register(new NimbusArraySpell());
 }
