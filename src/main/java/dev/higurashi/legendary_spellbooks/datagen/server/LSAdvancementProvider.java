e9647babbff570da16368dfeb4a113ecb4833401
Higurashi34m <240164450+Higurashi34m@users.noreply.github.com>
2026-06-16 09:41:33 +0900

    fix: fix an issue where the advancement description localization was not correctly applied

--- src/main/java/dev/higurashi/legendary_spellbooks/datagen/server/LSAdvancementProvider.java
+++ src/main/java/dev/higurashi/legendary_spellbooks/datagen/server/LSAdvancementProvider.java
@@ -50,7 +50,7 @@ public class LSAdvancementProvider extends ForgeAdvancementProvider {
 
             Advancement oblivionmancerArmorAdvancement = Advancement.Builder.advancement()
                     .parent(root)
-                    .display(LSItemRegistry.STORMMANCER_HOOD.get(), Component.translatable("advancement.legendary_spellbooks.stormmancer_armor.title"), Component.translatable("advancement.legendary_spellbooks.stormmancer_armor_description"), null, FrameType.CHALLENGE, true, true, false)
+                    .display(LSItemRegistry.STORMMANCER_HOOD.get(), Component.translatable("advancement.legendary_spellbooks.stormmancer_armor.title"), Component.translatable("advancement.legendary_spellbooks.stormmancer_armor.description"), null, FrameType.CHALLENGE, true, true, false)
                     .addCriterion("has_item", InventoryChangeTrigger.TriggerInstance.hasItems(LSItemRegistry.STORMMANCER_HOOD.get(), LSItemRegistry.STORMMANCER_ROBE.get(), LSItemRegistry.STORMMANCER_LEGGINGS.get(), LSItemRegistry.STORMMANCER_BOOTS.get()))
                     .save(saver, ResourceLocation.fromNamespaceAndPath(LegendarySpellbooks.MOD_ID, "main/get_stormmancer_armor"), helper);
         }
